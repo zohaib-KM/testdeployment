02cd57ef8694eb384d540a9400f12ffa3d52d28e
zohaib-KM <zohai.hamdani@kryptomind.com>
2024-08-20 15:12:12 +0500

    added video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import ShowData from "./components/ShowData";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Navbar />
-      <ShowData />
-    </>
+    <div className="App">
+      <div className="video-background">
+        <video autoPlay loop muted>
+          <source src="https://d2zgwwgmzrzanm.cloudfront.net/Whitehouse-carpet-and-wall-change.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="content">
+        <h1>Welcome to My React App</h1>
+      </div>
+    </div>
   );
 }
 
